c0b4694fa82636148a37e3de98e7f547d2c520c4
Vuppala Saketh Kumar <119433657+Sakethkumar31@users.noreply.github.com>
2025-07-24 20:18:20 +0530

    Add files via upload

---
+++ Arithematic.java
@@ -0,0 +1,10 @@
+public class Arithematic{
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Addition"+(a+b));
+        System.out.println("Subtraction"+(a-b));
+        System.out.println("Multiplication"+(a*b));
+        System.out.println("Division"+(a/b));
+    }
+}
